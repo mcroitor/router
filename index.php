48b6bf5c4c2f92ea657b8901ba9f5802da8d5904
Mihail Croitor <mcroitor@gmail.com>
2022-10-21 15:53:14 +0300

    test update

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "../src/mc/router.php";
+
+class post {
+    public static function all(array $params) { echo "show all posts"; }
+    public static function view(array $params) { echo "show post {$params[0]}"; }
+    public static function edit(array $params) { echo "edit post {$params[0]}"; }
+}
+
+$routes = [
+    "post/all" => "post::all",
+    "post/view" => "post::view",
+    "post/edit" => "post::edit",
+    "about" => function() { echo "about function called"; }
+];
+
+// register routes
+\mc\router::init($routes);
+
+// process route
+\mc\router::run();
